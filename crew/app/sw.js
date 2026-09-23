834777ef6386342a25b94fe35bd3feb0718ca243
myersmail9-afk <myersmail9@gmail.com>
2026-09-23 13:58:19 -0600

    auto-sync: 2026-09-23 13:58:19 (1 files)

--- crew/app/sw.js
+++ crew/app/sw.js
@@ -15,7 +15,7 @@
 // Do NOT bump it for content.js edits.
 
 const SHELL_CACHE = 'ttc-crew-v43';
-const FILES_CACHE = 'ttc-crew-files-v30';
+const FILES_CACHE = 'ttc-crew-files-v31';
 const SHELL = [
   './',
   './index.html',
